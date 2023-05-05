9119518d2f46185364e6851453c45574541d7e0e
Muxiner <muxinerhuang@gmail.com>
2023-05-05 12:44:33 +0800

    修改测试文件

--- tests/test_file/test_dir_d2/test_dir_d3/test_file_d3.c
+++ tests/test_file/test_dir_d2/test_dir_d3/test_file_d3.c
@@ -6,3 +6,29 @@ int main()
  
     return 0;
 }
+
+
+//[profile.dev]
+//opt-level = 0
+//
+//[profile.release]
+//opt-level = 3
+//codegen-units = 1
+//lto = true
+//strip = true
+//
+//
+//[dependencies]
+//structopt = "0.3.26"
+//termcolor = "1.2.0"
+//rayon = "1.6.1"
+//atty = "0.2.14"
+//assert_cmd = "2.0.8"
+//walkdir = "2"
+//
+//[target.'cfg(windows)'.dependencies]
+//winapi-util = "0.1.5"
+//
+//[target.'cfg(windows)'.dependencies.winapi]
+//version = "0.3.9"
+//features = ["winerror"]
